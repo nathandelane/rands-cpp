3d3797a6e76eef5f5eeb5690d55eebe8a50da09e
Nathan Lane <nathan.lane@overstock.com>
2015-10-08 12:57:13 -0600

    Moving constants out into file.

---
+++ Constants.h
@@ -0,0 +1,24 @@
+/*
+ * Constants.h
+ *
+ *  Created on: Oct 8, 2015
+ *      Author: nathanlane
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+namespace Nathandelane
+{
+
+	const std::string HexArgValue("hex");
+	const std::string HtmlFriendlyArgValue("htmlfriendly");
+	const std::string UniqueOnlyArgValue("uniqueonly");
+	const std::string AlphaOnlyArgValue("alphaonly");
+	const std::string AlphaNumericArgValue("alphanumeric");
+	const std::string AllAsciiPrintableValue("allasciiprintable");
+	const std::string NumStringsValue("numstrings");
+
+}
+
+#endif /* CONSTANTS_H_ */
